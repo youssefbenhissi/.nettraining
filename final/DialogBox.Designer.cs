52c67af2687b039a175f04d30229eb2f775ef503
Youssef Benhissi <youssef.benhissi@esprit.tn>
2023-03-03 01:26:04 +0200

    Title & Message for Dialog Box

---
+++ final/DialogBox.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace final
+{
+    partial class DialogBox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.yesButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // yesButton
+            // 
+            this.yesButton.Location = new System.Drawing.Point(385, 131);
+            this.yesButton.Name = "yesButton";
+            this.yesButton.Size = new System.Drawing.Size(75, 23);
+            this.yesButton.TabIndex = 0;
+            this.yesButton.Text = "YES";
+            this.yesButton.UseVisualStyleBackColor = true;
+            this.yesButton.Click += new System.EventHandler(this.yesButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(19, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "....";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 305);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "....";
+            // 
+            // DialogBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.yesButton);
+            this.Name = "DialogBox";
+            this.Text = "DialogBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button yesButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ final/DialogBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final
+{
+    public partial class DialogBox : Form
+    {
+        public DialogBox()
+        {
+            InitializeComponent();
+        }
+
+        private void yesButton_Click(object sender, EventArgs e)
+        {
+           DialogResult dr=  MessageBox.Show("1: To Purchase, Press = YES. \n 2: For Trial Version, Press = 2 \n 3: Cancel Order ", "Purchase Software", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information);
+            if(dr == DialogResult.Yes)
+            {
+                label2.Text = "Congratulations, you have purchased the complete software for unlimited";
+            }
+            else if ( dr == DialogResult.No)
+            {
+                label2.Text = "you can use this software for FREE for 30 days";
+            }
+            else if (dr == DialogResult.Cancel)
+            {
+                label1.Text = "You cancelled the order";
+            }
+        }
+    }
+}
