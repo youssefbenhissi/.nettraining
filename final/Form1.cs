a8305b850e1f49a52a82c52af4d83033405c3a2a
Youssef Benhissi <youssef.benhissi@esprit.tn>
2023-03-02 19:58:47 +0200

    Handle button actions

---
+++ final/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace final
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void setButton_Click(object sender, EventArgs e)
+        {
+            helloWorldLabel.Text = "Hello World Again  Again...";
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            helloWorldLabel.Text = "Text has been Cancelled ...";
+        }
+    }
+}
